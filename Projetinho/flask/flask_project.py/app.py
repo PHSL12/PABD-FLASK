ce334a09220c1d1489e4692909cc88c27eb79ad2
Pedro Henrique <phsl12.rf@gmail.com>
2024-01-07 01:31:01 -0300

    crud

--- Projetinho/flask/flask_project.py/app.py
+++ Projetinho/flask/flask_project.py/app.py
@@ -1,23 +1,38 @@
 # app.py
 from flask import Flask, render_template
-from database import get_books
+from database import get_books, create_book, update_book, remove_book, get_book_by_id
 
 app = Flask(__name__)
 
-@app.route('/create')
-def books_create():
-    books = get_books()
-    return render_template('create.html', books=books)
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+    if request.method == 'POST':
+        nome = request.form['nome']
+        data_inicio = request.form['data_inicio']
+        data_fim = request.form['data_fim']
+        create_book(nome, data_inicio, data_fim)
+        return redirect(url_for('books_list'))
+    return render_template('create.html')
 
-@app.route('/list')
-def books_list():
-    books = get_books()
-    return render_template('create.html', books=books)
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    book = get_book_by_id(id)
+    print(book)
+    if request.method == 'POST':
+        nome = request.form['nome']
+        data_inicio = request.form['data_inicio']
+        data_fim = request.form['data_fim']
+        update_book(id, nome, data_inicio, data_fim)
+        return redirect(url_for('_list'))
+    return render_template('update.html', book=book)
+
+@app.route('/remove/<int:id>', methods=['GET', 'POST'])
+def remove(id):
+    remove_book(id)
+    return redirect(url_for('books_list'))
 
 @app.route('/')
-def books():
+def books_list():
     books = get_books()
-    return render_template('base.html', books=books)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    print(books)
+    return render_template('index.html', books=books)
--- Projetinho/flask/flask_project.py/database.py
+++ Projetinho/flask/flask_project.py/database.py
@@ -4,7 +4,7 @@ from psycopg2 import sql
 
 DB_NAME = "books_database"
 USER = "postgres"
-PASSWORD = ""
+PASSWORD = "123"
 HOST = "localhost"
 PORT = "5432"
 
@@ -44,7 +44,7 @@ def criar_tabelas(sql):
 
 
 
-def insert_book(book_id, book_title, book_autor, book_year, book_gender):
+def create_book(book_id, book_title, book_autor, book_year, book_gender):
     with conectar_db() as conn:
         with conn.cursor() as cursor:
             cursor.execute(
@@ -66,6 +66,37 @@ def get_books():
     conn.close()
     return books
 
+def get_book_by_id(book_id):
+    conn = conectar_db()
+    book = None
+    with conn.cursor() as cursor:
+        cursor.execute("SELECT id, title, autor, year, gender FROM books WHERE id = %s", (book_id,))
+        row = cursor.fetchone()
+        if row:
+            book = {
+                'id': row[0],
+                'title': row[1],
+                'autor': row[2],
+                'year': row[3],
+                'gender': row[4],
+            }
+    return book
+
+def update_book(id, title, autor, year, gender):
+    with conectar_db() as conn:
+        with conn.cursor() as cursor:
+            cursor.execute(
+                "UPDATE books SET title = %s, autor = %s, year = %s, gender = %s WHERE id = %s",
+                (title, autor, year, gender, id)
+            )
+            conn.commit()
+
+def remove_book(id):
+    with conectar_db() as conn:
+        with conn.cursor() as cursor:
+            cursor.execute("DELETE FROM books WHERE id = %s", (id))
+            conn.commit()
+
 
 if __name__ == "__main__":
     criar_db()
@@ -81,5 +112,6 @@ if __name__ == "__main__":
     
     criar_tabelas(sql_create_books_table)
 
-    #insert_book(1, "Confissões", "Santo Agostinho", 397, "Teologia")
+    insert_book(1, "Confissões", "Santo Agostinho", 397, "Teologia")
     insert_book(2, "A Revolta de Atlas", "Ayn Rand", 1957, "Filosofia")
+
